adf7be162089e23a7666ec25ca6ec231e585b15e
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-03-08 16:53:15 +0100

    added conditional icon isLoaned color

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,23 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { FiBookOpen } from "react-icons/fi";
-import { ImBin2 } from "react-icons/im"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiBookOpen } from 'react-icons/fi';
+import { ImBin2 } from 'react-icons/im';
+import { BsFillPencilFill } from 'react-icons/bs';
+import styles from './bookCard.module.css';
 
 export default function BookCard({ item, deleteById }) {
-
-const handlerDelete = () => {
-  deleteById(item.id)
-}
+  const handlerDelete = () => {
+    deleteById(item.id);
+  };
 
   return (
     <div>
       <Link to={`/books/${item.id}`}>
-        <img src={item.coverUrl} alt="Cover Book"></img>
+        <img src={item.coverUrl} alt='Cover Book'></img>
       </Link>
-      <FiBookOpen />
-      <ImBin2 onClick={handlerDelete}/>
+      <FiBookOpen className={item.isLoaned ? styles.isLoanedActive : ''} />
+      <ImBin2 onClick={handlerDelete} />
+      <BsFillPencilFill />
 
-      <p>{item.title}</p>
+      <h3 className={styles.h3}>{item.title}</h3>
+      <h3>{item.author}</h3>
     </div>
   );
 }
