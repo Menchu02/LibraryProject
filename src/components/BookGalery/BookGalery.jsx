00e19a4068076931005e340adc2ea0537ec7cf07
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-03-27 14:14:59 +0200

    add function toogleIsLoaned

--- src/components/BookGalery/BookGalery.jsx
+++ src/components/BookGalery/BookGalery.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import apiService from '../../apiService/bookService';
+
 import BookCard from '../BookCard/BookCard';
 import styles from './bookGalery.module.css';
 
@@ -12,6 +13,24 @@ export default function BookGalery() {
     });
   }, []);
 
+  //FUNCIÓN PARA CAMBIAR EL ESTADO DEL ISLOANED:
+  const handleIsLoaned = (libro) => {
+    let editBook = { ...libro, isLoaned: !libro.isLoaned };
+
+    let newState = book.map((item) => (item.id === libro.id ? editBook : item));
+
+    apiService
+      .toggleIsLoaned(libro.id, { isLoaned: !libro.isLoaned })
+      .then(() => setBook(newState))
+
+      .catch((error) => console.error(error));
+
+    // bookService
+    //   .toggleFavorite(movie.id, { isFavorite: !movie.isFavorite })
+    //   .then(() => setMovies(newState))
+    //   .catch((error) => console.log(error));
+  };
+
   const deleteById = (idToDelete) => {
     apiService.deleteById(idToDelete);
     let newData = book.filter((item) => item.id !== idToDelete);
@@ -22,6 +41,7 @@ export default function BookGalery() {
     <div className={styles.containerGallery}>
       {book.map((item) => (
         <BookCard
+          handleIsLoaned={handleIsLoaned}
           key={item.id}
           title={item.title}
           item={item}
