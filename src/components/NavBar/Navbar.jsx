1ee972e8b7ef77c0251a03aba3fe1975d923cce4
Pere1J <pere1josep@gmail.com>
2023-04-05 14:29:00 +0200

    minor changes

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,36 +1,36 @@
-import React from 'react';
-import styles from './navBar.module.css';
-import logo from '../../assets/logo.png';
-import { Link } from 'react-router-dom';
-import { FaRegUser, FaUserSlash } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import styles from "./navBar.module.css";
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
+import { FaRegUser, FaUserSlash } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 export default function NavBar() {
   let navigate = useNavigate();
 
   const handleLogOut = () => {
-    localStorage.removeItem('login');
-    navigate('/');
+    localStorage.removeItem("login");
+    navigate("/");
   };
 
   return (
     <div className={styles.navContainer}>
       <div className={styles.logoContainer}>
-        <img className={styles.imgNavBar} src={logo} alt='logo' />
+        <img className={styles.imgNavBar} src={logo} alt="logo" />
         <h1>Library</h1>
       </div>
       <div className={styles.linkContainer}>
         <ul className={styles.ulContainer}>
-          <Link className={styles.links} to={'/'}>
+          <Link className={styles.links} to={"/"}>
             <li>Home</li>
           </Link>
-          <Link className={styles.links} to={'books/form/'}>
+          <Link className={styles.links} to={"books/form/"}>
             <li>Crear</li>
           </Link>
-          <Link className={styles.links} to={'books/disponibles'}>
+          <Link className={styles.links} to={"books/disponibles"}>
             <li>Disponibles</li>
           </Link>
-          <Link to={'books/login/'}>
+          <Link className={styles.links} to={"books/login/"}>
             <FaRegUser />
           </Link>
           <FaUserSlash onClick={handleLogOut} />
